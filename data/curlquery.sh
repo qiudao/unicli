7139112b5e5a953c34c223dfd1a57c45bc33d6fc
k <k@klqiudao.com>
2021-03-05 17:12:10 +0800

    curl get graph data

--- data/curlquery.sh
+++ data/curlquery.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then 
- echo "Usage: $0 <query-json>"
- exit
-fi
-
-echo "{"query":"query$1" }" 
-exit
 curl -g \
 		 -X POST \
 		 -H "Content-Type: application/json" \
-		 -d "{"query":"query$1" }" \
-			https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2
+		 -d '{"query":"query{uniswapFactories(first: 5) { id pairCount totalVolumeUSD totalVolumeETH untrackedVolumeUSD totalLiquidityUSD totalLiquidityETH txCount} }" }' \
+			https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2 | jq
